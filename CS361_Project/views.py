5b867a4b6366310ba8e1a9d4bebcd7fd29b112a9
Dan <danhle346@gmail.com>
2022-12-01 05:56:06 -0600

    Basic Home Views implement

--- CS361_Project/views.py
+++ CS361_Project/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import authenticate, login, logout
-from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
 from django.views import View
 from .models import Account, Supervisor
@@ -10,16 +10,26 @@ class Login(View):
         return render(request, "login.html")
 
     def post(self, request):
-        Username = request.POST.get("username")
+        Username = request.POST.get('username')
         Password = request.POST.get('password')
         try:
             user = Account.objects.get(username=Username)
         except:
             return render(request, "login.html", {"error": "User does not exist"})
         user = authenticate(request, username=Username, password=Password)
-
         if user is not None:
             login(request, user)
             return redirect('home/')
         else:
-            return render(request, "login.html", {"error": "Incorrect password"})
+            return render(request, "login.html", {"error": "Incorrect Password"})
+
+
+class Home(LoginRequiredMixin, View):
+    login_url = '/'
+    redirect_field_name = 'login'
+
+    def get(self, request):
+        return render(request, "Home.html")
+
+    def post(self, request):
+        pass
