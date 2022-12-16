a7f21ba5ebb1bcbf25ee47c0055f52d02d2d8ca0
Dinossiah <117776828+Dinossiah@users.noreply.github.com>
2022-12-16 17:03:13 -0600

    Implementation of the Create, Edit and Delete Account

--- CS361_Project/views.py
+++ CS361_Project/views.py
@@ -2,7 +2,7 @@ from django.middleware.csrf import rotate_token
 from django.shortcuts import render, redirect
 from django.views import View
 from .models import Account, Supervisor, Instructor, TA, Course, LabSection
-from .functions import generateID
+from .functions import generateID, changeName, changePassword, changeEmail, changeAddress,changeTelephone, passwordChecker
 
 
 # Create your views here.
@@ -54,10 +54,113 @@ class Home(View):
 
 class ManageAccounts(View):
     def get(self, request):
+        request.session['action'] = None
+        return render(request, 'Accounts/Manage.html', {"validForm": 'invalid'})
+
+    def post(self, request):
+        request.session['action'] = None
+        if request.POST.get('create') is not None:
+            request.session['action'] = 'create'
+        elif request.POST.get('edit') is not None:
+            request.session['action'] = 'edit'
+        else:
+            request.session['action'] = 'delete'
+
+        return render(request, 'Accounts/Manage.html')
+
+
+class CreateAccount(View):
+
+    def get(self, request):
+        return render(request, 'Accounts/Manage.html', {"validForm": "valid"})
+
+    def post(self, request):
+        action = request.session["action"]
+
+        if action == "create":
+            username = request.POST.get("Username")
+            if  len(list(Account.objects.filter(username = username))) > 0 :#username already exists
+                error = "Username already exists in Database"
+                return render(request, 'Accounts/Manage.html',{"error":error})
+
+            role = request.POST.get("role")
+            if role == None:#No role given, the user requires a role
+                error = "Every user needs to have a role"
+                return render(request, "Accounts/Manage.html", {"error":error})
+
+            password = request.POST.get("Password")
+            if passwordChecker(password) == False: #Bad Password
+                error = "Password must have at least one digit, one upper case character, one lower case character, one special symbol, and at least 5 characters"
+                return render(request,"Accounts/Manage.html", {"error":error})
+
+            id = generateID(username)
+            newAccount = Account(id = id, username = username,
+                                 password = password,
+                                 name = request.POST.get("Name"),
+                                 role = role,
+                                 email = request.POST.get("Email"),
+                                 telephone = request.POST.get("Telephone"),
+                                 address = request.POST.get("Address")
+            )
+            newAccount.save()
+        return render(request, 'Accounts/Manage.html')
+
+
+class EditAccount(View):
+
+    def get(self, request):
         return render(request, 'Accounts/Manage.html')
 
     def post(self, request):
-        pass
+        action = request.session["action"]
+
+        if action == "edit":
+            username = request.POST.get("Username")
+            if len(list(Account.objects.filter(username = username))) == 0 :  # username doesn't exists
+                error = "Username not in Database"
+                return render(request, 'Accounts/Manage.html',{"error":error})
+
+            editAccount = Account.objects.get(username=username)
+            if request.POST.get("Password") != "":
+                if passwordChecker(request.POST.get("Password")) == False:  # Bad Password
+                    error = "Password must have at least one digit, one upper case character, one lower case character, one special symbol, and at least 5 characters"
+                    return render(request, "Accounts/Manage.html", {"error": error})
+                changePassword(editAccount, request.POST.get("Password"))
+
+            if request.POST.get("Name") != "":
+                changeName(editAccount, request.POST.get("Name"))
+
+            if request.POST.get("Email") != "":
+                changeEmail(editAccount, request.POST.get("Email"))
+
+            if request.POST.get("Telephone") != "":
+                changeTelephone(editAccount, request.POST.get("Telephone"))
+
+            if request.POST.get("Address") != "":
+                changeAddress(editAccount, request.POST.get("Address"))
+
+            editAccount.save()
+
+        return render(request, 'Accounts/Manage.html')
+
+
+class DeleteAccount(View):
+
+    def get(self, request):
+        return render(request, 'Accounts/Manage.html')
+
+    def post(self, request):
+        action = request.session["action"]
+
+        if action == "delete":  #delete
+            username = request.POST.get("username")
+            if len(list(Account.objects.filter(username = username))) == 0 :  # username doesn't exists
+                error = "Username not in Database"
+                return render(request, 'Accounts/Manage.html',{"error":error})
+            else:
+                deleteAccount = Account.objects.get(username=username)
+                deleteAccount.delete()
+        return render(request, 'Accounts/Manage.html')
 
 
 class Notification(View):
