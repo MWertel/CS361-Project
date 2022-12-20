23d2a23b7084eaa482f796c0e28cc60620c74fd8
Danu <danhle346@gmail.com>
2022-12-20 08:14:52 -0600

    Turn Account object into a formatted Dictionary.

---
+++ SuperLooper/modelDict.py
@@ -0,0 +1,10 @@
+def Account_Dict(modelObj):
+    result = {
+        "username": modelObj.username,
+        "name": modelObj.name,
+        "role": modelObj.role,
+        "email": modelObj.email,
+        "address": modelObj.address,
+        "telephone": modelObj.telephone
+    }
+    return result
